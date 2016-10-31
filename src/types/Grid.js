da1f4d0138be5cacd177ea3c6ed2f030e4870c17
Claudio Brandolino <cbrandolino@gmail.com>
2016-10-31 21:03:44 +0100

    Added Flow type checks

--- src/types/Grid.js
+++ src/types/Grid.js
@@ -1,5 +1,11 @@
-class Grid {
-  initialize(width, height) {
+//@flow
 
+class Grid {
+  width: number;
+  height:number;
+  initialize(width:number, height:number) {
+    this.width = width;
+    this.height = height;
   }
+
 }
