7f5f73613e7432c130dda36346a21fe8974b3f25
feychou <federica.recanatini@gmail.com>
2016-10-31 16:08:26 +0100

    Create Letterbox skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import LetterBox from './components/LetterBox';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Ouija</h1>
+        <LetterBox />
       </div>
     );
   }
---
+++ src/components/LetterBox/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function LetterBox () {
+  return (
+    <div className="LetterBox">
+    </div>
+  )
+}
