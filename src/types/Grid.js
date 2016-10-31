8278db7bcdc3f8ad9f5fce2429085cef62ee34b1
Claudio Brandolino <cbrandolino@gmail.com>
2016-10-31 23:02:48 +0100

    Refactored nodes and vertices

--- src/types/Grid.js
+++ src/types/Grid.js
@@ -1,12 +1,18 @@
 //@flow
-import { Range } from 'immutable';
+import { Range, Seq, Map } from 'immutable';
+import R from 'ramda';
 
 class Grid {
-  rows:   [number];
-  cols:   [number];
+  rows: Seq.Indexed;
+  cols: Seq.Indexed;
+  nodes: Array<Set<number>>;
+  vertices: Map<Set<number>, bool>;
 
   initialize(w:number, h:number) {
     this.rows = Range(1, w);
     this.cols = Range(1, h);
+    this.nodes = R.chain(new Set, this.cols, this.rows);
   }
 }
+
+export default Grid;
--- src/types/Grid.test.js
+++ src/types/Grid.test.js
@@ -1,12 +1,13 @@
 import Grid from './Grid';
+
 const a = new Grid(3, 8);
 const b = new Grid(4, 4);
 
-describe('#initialize', () => {
+test('#initialize', () =>{
   it('sets rows and columns', () => {
-    expect(a.rows).to.eql([1, 2, 3]);
-    expect(a.cols).to.eql([1, 2, 3, 4, 5, 6, 7, 8]);
-    expect(b.rows).to.eql([1, 2, 3, 4]);
-    expect(b.cols).to.eql([1, 2, 3, 4]);
+    expect(a.rows).toEqual([1, 2, 3]);
+    expect(a.cols).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+    expect(b.rows).toEqual([1, 2, 3, 4]);
+    expect(b.cols).toEqual([1, 2, 3, 4]);
   });
 });
