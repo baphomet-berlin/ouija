33ae74c0d3c9db36b715a5a2913a5a659e68e3fa
Claudio Brandolino <cbrandolino@gmail.com>
2016-11-01 01:17:17 +0100

    Fixed nodes + vertices

--- src/types/Grid.js
+++ src/types/Grid.js
@@ -1,17 +1,29 @@
 //@flow
-import { Range, Seq, Map } from 'immutable';
+import { Range, Seq, List, Map, Set } from 'immutable';
 import R from 'ramda';
 
 class Grid {
   rows: Seq.Indexed;
   cols: Seq.Indexed;
-  nodes: Array<Set<number>>;
-  vertices: Map<Set<number>, bool>;
+  nodes: List<List<number>>;
+  vertices: Map<Set<List<number>>, bool>;
 
-  initialize(w:number, h:number) {
-    this.rows = Range(1, w);
-    this.cols = Range(1, h);
-    this.nodes = R.chain(new Set, this.cols, this.rows);
+  constructor(w:number, h:number) {
+    this.rows = Range(1, w + 1);
+    this.cols = Range(1, h + 1);
+    this.nodes = this.cols.flatMap(col => this.rows.map(row => List.of(row, col))).toList();
+    const combinations = this.nodes.map(a => 
+      this.nodes.map(b => [a, b]))
+    // this.vertices = new Map(combinations.filter(it => 
+    //   this.isValidVertex(it.first(), it.last())).map(it =>
+    //      [Set(it), false]
+    // ))
+
+    //this.vertices = R.map(this.isValidVertex, this.nodes, this.nodes)
+  }
+
+  isValidVertex(a:List<number>, b:List<number>) {
+    return a != b && (b.first() - a.first() <= 1) && (b.last() - a.last() <= 1) && (a.first() <= b.first()) && (a.last() <= b.last());
   }
 }
 
--- src/types/Grid.test.js
+++ src/types/Grid.test.js
@@ -1,13 +1,22 @@
 import Grid from './Grid';
+import { List, Range } from 'immutable';
 
-const a = new Grid(3, 8);
-const b = new Grid(4, 4);
-
-test('#initialize', () =>{
+describe('#constructor', () =>{
+  const a = new Grid(3, 8);
+  const b = new Grid(4, 4);
   it('sets rows and columns', () => {
-    expect(a.rows).toEqual([1, 2, 3]);
-    expect(a.cols).toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
-    expect(b.rows).toEqual([1, 2, 3, 4]);
-    expect(b.cols).toEqual([1, 2, 3, 4]);
+    expect(a.rows).toEqual(Range(1, 4));
+    expect(a.cols).toEqual(Range(1, 9));
+    expect(b.rows).toEqual(Range(1, 5));
+    expect(b.cols).toEqual(Range(1, 5));
+  });
+  it('sets nodes', () => {
+    expect(a.nodes.size).toBe(24);
+    expect(a.nodes.first).toBe(List(1,1));
+  });
+
+  it('sets vertices', () => {
+    // expect(a.vertices.size).toBe(51);
+    // expect(b.vertices.size).toBe(42);
   });
 });
