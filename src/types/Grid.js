bb5b7036c830d55dfa428840ac956f2490ffec1e
Claudio Brandolino <cbrandolino@gmail.com>
2016-11-03 00:14:44 +0100

    Refactored grid arguments and points

--- src/types/Grid.js
+++ src/types/Grid.js
@@ -14,23 +14,22 @@ class Grid {
   successors: Array<Array<number>>;
 
   constructor(w:number, h:number) {
-    this.rows = Range(1, w + 1);
-    this.cols = Range(1, h + 1);
-    const successors = [[0, -1], [0, 1], [1, 0], [1, 1]];
-    this.nodes = this.cols.flatMap(col => 
-      this.rows.map(row =>
-        List.of(row, col)
-      )
-    ).toSet();
-    this.vertices = this.nodes.reduce((acc, node) => {
-      const nodeSuccessors = successors
-        .map(([x, y]) => [x + node.first(), y + node.last()])
-        .filter(([x, y]) => 
-          (x <= this.rows.last() && x >= this.rows.first()) && 
-          (y <= this.cols.last() && x >= this.rows.first()))
-        .map(([x, y]) => [node, List.of(x, y), false]);
-      return acc.union(Set(nodeSuccessors));
-    }, Set());
+    this.xPoints = w + 1;
+    this.yPoints = h + 1;
+    const successors = [[1, -1], [0, 1], [1, 0], [1, 1]];
+    this.nodes = Range(0, this.xPoints).flatMap(col => 
+      Range(0, this.yPoints).map(row =>
+        List.of(col, row)
+    )).toSet();
+    // this.vertices = this.nodes.reduce((acc, node) => {
+    //   const nodeSuccessors = successors
+    //     .map(([x, y]) => [x + node.first(), y + node.last()])
+    //     .filter(([x, y]) => 
+    //       (x <= this.cols.last() && x >= this.cols.first()) && 
+    //       (y <= this.rows.last() && x >= this.rows.first()))
+    //     .map(([x, y]) => [node, List.of(x, y), false]);
+    //   return acc.union(Set(nodeSuccessors));
+    // }, Set());
  }
 
   isValidVertex(a:List<number>, b:List<number>) {
--- src/types/Grid.test.js
+++ src/types/Grid.test.js
@@ -3,23 +3,22 @@ import { List, Range } from 'immutable';
 
 describe('#constructor', () =>{
   const oneByOne = new Grid(1, 1);
-  const a = new Grid(3, 8);
-  const b = new Grid(4, 4);
-  it('sets rows and columns', () => {
-    expect(a.rows).toEqual(Range(1, 4));
-    expect(a.cols).toEqual(Range(1, 9));
-    expect(b.rows).toEqual(Range(1, 5));
-    expect(b.cols).toEqual(Range(1, 5));
+  const gridFont = new Grid(2, 6);
+  const fourSquare = new Grid(4, 4);
+  it('sets x and y points', () => {
+    expect(oneByOne.xPoints).toEqual(2);
+    expect(oneByOne.yPoints).toEqual(2);
+    expect(gridFont.xPoints).toEqual(3);
+    expect(gridFont.yPoints).toEqual(7);
   });
   it('sets nodes', () => {
-    expect(a.nodes.size).toBe(24);
-    expect(a.nodes.first()).toEqual(List.of(1, 1));
-    expect(a.nodes.last()).toEqual(List.of(3, 8));
+    expect(oneByOne.nodes.size).toBe(4);
+    expect(gridFont.nodes.size).toBe(3 * 7);
   });
 
-  it('sets vertices', () => {
-    expect(oneByOne.vertices.size).toBe(6)
-    expect(a.vertices.size).toBe(59);
-    expect(b.vertices.size).toBe(37);
-  });
+  // it('sets vertices', () => {
+  //   expect(oneByOne.vertices.size).toBe(6);
+  //   expect(oneByOne.vertices.size).toBe(59);
+  //   expect(gridFont.vertices.size).toBe(37);
+  // });
 });
