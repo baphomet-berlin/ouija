5bd9501b09981870c60138de9b6c77dc15b34b4e
Claudio Brandolino <cbrandolino@gmail.com>
2016-11-04 23:59:48 +0100

    Added points from grid type

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,14 @@
+//@flux
+
 import React from 'react';
 import LetterBox from './LetterBox';
+import Grid from '../types/Grid';
 
 export default function Board ({letters}) {
   return (
     <div className="Alphabet">
       {letters.split('').map(letter => (
-        <LetterBox key={letter} letter={letter} />
+        <LetterBox key={letter} letter={letter} grid={new Grid(2, 6)} />
       ))}
     </div>
   )
