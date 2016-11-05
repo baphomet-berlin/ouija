b8daa639a1b2b52f5ec10b829b64bd423e18db4d
feychou <federica.recanatini@gmail.com>
2016-11-05 19:56:06 +0100

    Change editor to statefulleable component

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,28 +1,33 @@
 //@flux
 
-import React from 'react';
+import React, {Component} from 'react';
 
 import LetterBox from './LetterBox';
 import Grid from '../types/Grid';
 
 import './Editor.css';
 
-export default function Editor({letters}) {
-  return (
-    <div className="Editor">
-      <span className="SaveButton"></span>
-      <div className="Alphabet">
-        {letters.split('').map(letter => (
-          <LetterBox
-            key={letter}
-            letter={letter}
-            grid={new Grid(2, 6)} />
-        ))}
+class Editor extends Component {
+  render() {
+    const {letters} = this.props;
+    return (
+      <div className="Editor">
+        <span className="SaveButton"></span>
+        <div className="Alphabet">
+          {letters.split('').map(letter => (
+            <LetterBox
+              key={letter}
+              letter={letter}
+              grid={new Grid(2, 6)} />
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 Editor.defaultProps = {
   letters: 'abcdefghijklmnopqrstuvwxyz'
 }
+
+export default Editor;
