bf8f3df49948c4415c60542c2febb4b8bd9de4a0
feychou <federica.recanatini@gmail.com>
2016-11-05 19:52:21 +0100

    Create save button

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,18 +1,24 @@
 //@flux
 
 import React from 'react';
+
 import LetterBox from './LetterBox';
 import Grid from '../types/Grid';
 
+import './Editor.css';
+
 export default function Editor({letters}) {
   return (
-    <div className="Alphabet">
-      {letters.split('').map(letter => (
-        <LetterBox
-          key={letter}
-          letter={letter}
-          grid={new Grid(2, 6)} />
-      ))}
+    <div className="Editor">
+      <span className="SaveButton"></span>
+      <div className="Alphabet">
+        {letters.split('').map(letter => (
+          <LetterBox
+            key={letter}
+            letter={letter}
+            grid={new Grid(2, 6)} />
+        ))}
+      </div>
     </div>
   )
 }
